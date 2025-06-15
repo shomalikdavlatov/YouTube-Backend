59ae9cfcbfd921afefd70803d2204fcd0e2835ff
shomalikdavlatov <shomalikdavlatov@gmail.com>
2025-06-15 21:50:12 +0500

    File structure created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { VideosModule } from './modules/videos/videos.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ChannelsModule } from './modules/channels/channels.module';
+import { SubscriptionsModule } from './modules/subscriptions/subscriptions.module';
+import { CommentsModule } from './modules/comments/comments.module';
+import { PlaylistsModule } from './modules/playlists/playlists.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [VideosModule, AuthModule, ChannelsModule, SubscriptionsModule, CommentsModule, PlaylistsModule, UsersModule],
   controllers: [],
   providers: [],
 })
